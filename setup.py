a5f0dcfeaab323ba355e2683b5d636db74452eb2
RaphaelMaser <raphaelmaser@gmail.com>
2023-06-18 13:36:26 +0200

    removed nnUnet source code and added dependency

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = '0.1.9'
-DESCRIPTION = 'A deep learnign based method to segment deep brain structures from T1w MRI'
+VERSION = '0.2.0'
+DESCRIPTION = 'A deep learning based method to segment deep brain structures from T1w MRI'
 # Setting up
 setup(
       name="DBSegment",
@@ -28,31 +28,15 @@ setup(
                         "requests==2.28.2 ",
                         "tifffile==2023.3.21", 
                         "matplotlib==3.7.1",
-                        "antspyx==0.3.8"],
+                        "antspyx==0.3.8",
+                        "nnunet==1.7.1",
+                        ],
       entry_points={
       'console_scripts': [
                           "DBSegment = DBSegment.DBSegment:main",
                           "DBSegment_prep = DBSegment.DBSegment:main_preprocess",
                           "DBSegment_infer = DBSegment.DBSegment:main_infer",
                           "DBSegment_bm = DBSegment.DBSegment:main_brainmask",
-                          'nnUNet_convert_decathlon_task = DBSegment.nnunet.experiment_planning.nnUNet_convert_decathlon_task:main',
-                          'nnUNet_plan_and_preprocess = DBSegment.nnunet.experiment_planning.nnUNet_plan_and_preprocess:main',
-                          'nnUNet_train = DBSegment.nnunet.run.run_training:main',
-                          'nnUNet_train_DP = DBSegment.nnunet.run.run_training_DP:main',
-                          'nnUNet_train_DDP = DBSegment.nnunet.run.run_training_DDP:main',
-                          'nnUNet_predict = DBSegment.nnunet.inference.predict_simple:main',
-                          'nnUNet_ensemble = DBSegment.nnunet.inference.ensemble_predictions:main',
-                          'nnUNet_find_best_configuration = DBSegment.nnunet.evaluation.model_selection.figure_out_what_to_submit:main',
-                          'nnUNet_print_available_pretrained_models = DBSegment.nnunet.inference.pretrained_models.download_pretrained_model:print_available_pretrained_models',
-                          'nnUNet_print_pretrained_model_info = DBSegment.nnunet.inference.pretrained_models.download_pretrained_model:print_pretrained_model_requirements',
-                          'nnUNet_download_pretrained_model = DBSegment.nnunet.inference.pretrained_models.download_pretrained_model:download_by_name',
-                          'nnUNet_download_pretrained_model_by_url = DBSegment.nnunet.inference.pretrained_models.download_pretrained_model:download_by_url',
-                          'nnUNet_determine_postprocessing = DBSegment.nnunet.postprocessing.consolidate_postprocessing_simple:main',
-                          'nnUNet_export_model_to_zip = DBSegment.nnunet.inference.pretrained_models.collect_pretrained_models:export_entry_point',
-                          'nnUNet_install_pretrained_model_from_zip = DBSegment.nnunet.inference.pretrained_models.download_pretrained_model:install_from_zip_entry_point',
-                          'nnUNet_change_trainer_class = DBSegment.nnunet.inference.change_trainer:main',
-                          'nnUNet_evaluate_folder = DBSegment.nnunet.evaluation.evaluator:nnunet_evaluate_folder',
-                          'nnUNet_plot_task_pngs = DBSegment.nnunet.utilities.overlay_plots:entry_point_generate_overlay',
                           ] },
       keywords=['python'],
       classifiers=[
